cb45e68a493b06ed2add75c40d1fc713ee5a5d44
Erez Porat <erezporatwork@gmail.com>
2023-07-16 22:02:02 +0300

    file update

---
+++ Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    intrernal class Reservation
+    {
+        private Client client;
+        private Room room;
+        private Payment payment;
+
+        public Reservation(Client client, Room room, Payment payment)
+        {
+            this.client = client;
+            this.room = room;
+            this.payment = payment;
+
+        }
+
+    }
+
+}
--- Room.cs
+++ Room.cs
@@ -4,8 +4,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace program
-{ 
+{
 
+    internal class Room
+   
+    {
+        private int roomNumber;
+        private bool isFree;
+        private int floor;
+
+        public Room(int roomNumber ,  int floor)
+       {
+            this.roomNumber = roomNumber;
+            this.floor = floor;
+            this.isFree = false;
+        }
 
 
 
